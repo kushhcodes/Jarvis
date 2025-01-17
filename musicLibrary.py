a87b92b1f78449368a42db3d83c7d4e6e7c25419
Khushal Patil <khushalpatil0405@gmail.com>
2025-01-18 01:26:27 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "skyfall" : "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "kids" : "https://www.youtube.com/watch?v=Ha2OcL_0gtM&pp=ygUUa2lkcyBzdHJhbmdlciB0aGluZ3M%3D",
+    "superhero" : "https://www.youtube.com/watch?v=_WCD3Z9UmJ4&pp=ygUJc3VwZXJoZXJv",
+    "sweater" : "https://www.youtube.com/watch?v=GCdwKhTtNNw&pp=ygUPc3dlYXRlciB3ZWF0aGVy",
+    "snowman" :"https://www.youtube.com/watch?v=gset79KMmt0&pp=ygUHc25vd21hbg%3D%3D"
+}
